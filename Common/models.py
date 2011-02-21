5bc111240877520a3d517771d84ca1329da64420
Kari <truongsinh.tran@gmail.com>
2011-02-21 14:35:18 +0200

    max-lengh = 127

--- Common/models.py
+++ Common/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Community(models.Model):
-	address = models.CharField(max_length=None)
+	address = models.CharField(max_length=127)
 
 class Apartment(models.Model):
 	community = models.ForeignKey(Community)
-	address = models.CharField(max_length=None)
+	address = models.CharField(max_length = 127)
 	note = models.TextField()
 
 class Profile(models.Model):
 	apartment = models.ForeignKey(Apartment)
-	plate_no = models.CharField(max_length=None)
+	plate_no = models.CharField(max_length = 127)
