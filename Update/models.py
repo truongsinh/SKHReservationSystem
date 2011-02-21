2129b2eeb6d221a8a1f02013cf1b6cba449e583a
Kari <truongsinh.tran@gmail.com>
2011-02-21 13:49:53 +0200

    app Update

--- Update/models.py
+++ Update/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db.models import *
 
 # Create your models here.
+class version(Model):
+	major = PositiveSmallIntegerField()
+	minor = PositiveSmallIntegerField()
+	STATUS_CHOICE = (
+		(0, 'dev'),
+		(1, 'alpha'),
+		(2, 'beta'),
+		(3, 'rc'),
+		(4, 'gold'),
+	)
+	status = PositiveSmallIntegerField(choices=STATUS_CHOICE)
+	build = DateField()
