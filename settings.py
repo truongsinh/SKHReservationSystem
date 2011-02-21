aca6fc4692857998d7edfd8f85e09217213f4c2c
Kari <truongsinh.tran@gmail.com>
2011-02-21 14:08:46 +0200

    universal template dir name

--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -77,17 +77,22 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'SKHReseravationSystem.urls'
 
-TEMPLATE_DIRS = ('/Users/truongsinh/PycharmProjects/SKHReseravationSystem/templates',)
+import os
+dirnames = os.path.dirname(globals()["__file__"])
+TEMPLATE_DIRS = (
+	os.path.join(dirnames, 'templates'),)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'SKHReseravationSystem.Common',
     'SKHReseravationSystem.Parking',
+    'SKHReseravationSystem.Update',
 )
