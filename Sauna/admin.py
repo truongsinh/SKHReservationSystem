526a7f9f1654adf36cc14373a80704d924a9875c
unknown <Jussi@.(none)>
2011-05-05 10:05:22 +0300

    Sauna, admin

--- Sauna/admin.py
+++ Sauna/admin.py
@@ -1,4 +1,4 @@
-from Sauna.models import Sauna, Time_slot
+from Sauna.models import Sauna, Time_slot, Sauna_queue, Sauna_transaction
 from django.contrib import admin
 
 class SaunaAdmin(admin.ModelAdmin):
@@ -24,6 +24,31 @@ class TimeAdmin(admin.ModelAdmin):
 	  			]
 
 
+class S_QueueAdmin(admin.ModelAdmin):
+	list_display = ('community', 'user', 'register_date', 'note')
+	list_filter = ('community__address', 'community__city', 'community__postal_code')
+	search_fields = ('community__city', 'community__address', 'community__postal_code', 'user__last_name', 'user__first_name')
 
+	fieldsets = [
+			('Sauna queue information',		{'fields': ['user','community',]}),
+			('Additional information', 		{'fields': ['note',]}),
+
+	  			]
+
+class S_TransactionAdmin(admin.ModelAdmin):
+	#list_display = ('sauna_slot', 'sauna_queue__community__name', 'sauna_queue__user__name', 'start_date', 'end_date',)
+	list_filter = ('sauna_queue__community__city', 'end_date',)
+	#Display doesn't work because it sucks
+
+	fieldsets = [
+			('Timeslot information',		{'fields': ['sauna_slot',]}),
+			('Queue information', 			{'fields': ['sauna_queue',]}),
+			('Date information',			{'fields': ['end_date', ]}),
+			('Additional information', 		{'fields': ['note',]}),
+
+	  			]
+	
 admin.site.register(Sauna, SaunaAdmin)
 admin.site.register(Time_slot, TimeAdmin)
+admin.site.register(Sauna_queue, S_QueueAdmin)
+admin.site.register(Sauna_transaction, S_TransactionAdmin)
--- Sauna/models.py
+++ Sauna/models.py
@@ -17,3 +17,28 @@ class Time_slot(models.Model):
 		return self.name
 	#This model need modification to bind certain user on it
 	#Similar to Parking_Slot model
+
+class Sauna_queue(models.Model):
+	user = models.ForeignKey(Profile, null=True)
+	community = models.ForeignKey(Community, null=True)
+	register_date = models.DateField(#'Registered Date',
+									 auto_now_add=True)
+	decision_date = models.DateField(#'Decided Date',
+									 blank=True, null=True)
+	note = models.TextField(blank=True)
+	def __unicode__(self):
+		return u'%s - %s' % (self.user, self.community)
+
+class Sauna_transaction(models.Model):
+	sauna_queue = models.OneToOneField(Sauna_queue,
+										 limit_choices_to = {'decision_date': None}
+										 )
+	sauna_slot = models.ForeignKey(Time_slot,
+								#	 limit_choices_to = {'free': True}
+									 )
+	start_date = models.DateField('Start Date', auto_now_add=True)
+	end_date = models.DateField('End Date', blank=True, null=True)
+	note = models.TextField(blank=True)
+
+	def __unicode__(self):
+		return u'%s - %s' % (self.sauna_slot, self.sauna_queue)
